c4952a03ccecf4e70ab2b68d1d2c7cbb39319150
e200 <eleandro@inbox.ru>
2018-12-23 22:41:35 +0100

    Added ImageController

---
+++ src/Http/Controllers/ImageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace e200\Mediavel\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use e200\Mediavel\Models\MimeType;
+use e200\Mediavel\Http\Requests\ImageRequest;
+
+class ImageController extends Controller
+{
+    // :/
+}
