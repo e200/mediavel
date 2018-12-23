94a0a6d6c21d4de86c5f4b0d060ee364c7e3b504
e200 <eleandro@inbox.ru>
2018-12-23 22:41:58 +0100

    Added mediavel service provider

---
+++ src/MediavelServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace e200\Mediavel;
+
+use Illuminate\Support\ServiceProvider;
+
+class MediavelServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'e200');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'e200');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/Http/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/mediavel.php', 'mediavel');
+
+        // Register the service the package provides.
+        $this->app->singleton('mediavel', function ($app) {
+            return new Mediavel;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['mediavel'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole()
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/mediavel.php' => config_path('mediavel.php'),
+        ], 'mediavel.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/e200'),
+        ], 'mediavel.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/e200'),
+        ], 'mediavel.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/e200'),
+        ], 'mediavel.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
