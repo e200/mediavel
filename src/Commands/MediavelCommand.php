821683740f01d4a510922fd1b846149242d0ea92
Eleandro Duzentos <eleandro@inbox.ru>
2018-12-24 22:42:00 +0100

    Added MediavelCommand

---
+++ src/Commands/MediavelCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace e200\Mediavel\Commands;
+
+use Illuminate\Console\Command;
+
+class MediavelCommand extends Command {
+
+    protected $signature = 'mediavel:handle';
+
+    protected $description = 'Bootstraps the Mediavel package';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function handle() {
+        //
+    }
+
+}
