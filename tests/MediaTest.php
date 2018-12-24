42337efd60a2a554cde2869788c6313e63a81fd1
Eleandro Duzentos <eleandro@inbox.ru>
2018-12-24 22:37:37 +0100

    Added test for store() Added getInstance()

--- tests/MediaTest.php
+++ tests/MediaTest.php
@@ -4,37 +4,75 @@ namespace e200\Mediavel\Tests;
 
 use Mockery;
 use e200\Mediavel\Media;
-use e200\Mediavel\Models\Media as MediaModel;
+use Illuminate\Http\UploadedFile;
 use Orchestra\Testbench\TestCase;
+use e200\Mediavel\Models\MimeType;
+use e200\Mediavel\MediavelServiceProvider;
+use e200\Mediavel\Models\Media as MediaModel;
 
 class MediaTest extends TestCase
 {
+    public function testStore()
+    {
+        $mimeType = Mockery::mock(MimeType::class)
+            ->shouldIgnoreMissing();
+
+        $mimeType
+            ->shouldReceive('firstOrCreate')
+            ->with(Mockery::any())
+            ->andReturns($mimeType);
+
+        $mimeType->id = 1;
+
+        $mediaModel = Mockery::mock(MediaModel::class);
+
+        $mediaModel
+            ->shouldReceive('create')
+            ->with(Mockery::any())
+            ->andReturns($mediaModel);
+
+        $media = $this->getInstance($mediaModel);
+
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        $this->assertEquals($media, $media->store($file, $mimeType));
+    }
+
     public function testBackup()
     {
-        $media = new Media(Mockery::mock(MediaModel::class));
+        $media = $this->getInstance();
 
         $this->assertEquals($media, $media->backup());
     }
 
     public function testOptimize()
     {
-        $media = new Media(Mockery::mock(MediaModel::class));
+        $media = $this->getInstance();
 
         $this->assertEquals($media, $media->optimize());
     }
 
     public function testResize()
     {
-        $media = new Media(Mockery::mock(MediaModel::class));
+        $media = $this->getInstance();
 
         $this->assertEquals($media, $media->resize(0, 0));
     }
 
     public function testToCollection()
     {
-        $media = new Media(Mockery::mock(MediaModel::class));
+        $media = $this->getInstance();
 
         $this->assertEquals($media, $media->toCollection('name'));
+    }
+
+    protected function getInstance($mediaMock = null)
+    {
+        if (is_null($mediaMock)) {
+            $mediaMock = Mockery::mock(MediaModel::class);
+        }
+
+        return new Media($mediaMock);
     }
 
     protected function tearDown()
