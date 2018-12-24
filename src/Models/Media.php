af4c6cbd28309da023d67f6990554f6d88d7b09b
Eleandro Duzentos <eleandro@inbox.ru>
2018-12-24 22:45:52 +0100

    Added mime_type_id to $fillable
    Added table name

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -2,12 +2,21 @@
 
 namespace e200\Mediavel\Models;
 
+use e200\Mediavel\Models\MimeType;
 use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
+    protected $table = 'medias';
+
     protected $fillable = [
         'client_name',
-        'file_name'
+        'file_name',
+        'mime_type_id'
     ];
+
+    public function mimeType()
+    {
+        return $this->belongsTo(MimeType::class);
+    }
 }
