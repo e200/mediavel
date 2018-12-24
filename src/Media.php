965c4ae4b3a7010c7290b912f105d36915e31275
Eleandro Duzentos <eleandro@inbox.ru>
2018-12-24 22:41:16 +0100

    Updated contract rules Added store()

--- src/Media.php
+++ src/Media.php
@@ -2,34 +2,53 @@
 
 namespace e200\Mediavel;
 
-use e200\Mediavel\Models\Media as MediaModel;
+use Illuminate\Http\UploadedFile;
+use e200\Mediavel\Models\MimeType;
 use e200\Mediavel\Contracts\MediaInterface;
+use e200\Mediavel\Models\Media as MediaModel;
 
 class Media implements MediaInterface
 {
-    protected $model;
+    protected $mediaModel;
 
-    public function __construct(MediaModel $model)
+    public function __construct(MediaModel $mediaModel)
     {
-        $this->model = $model;
+        $this->mediaModel = $mediaModel;
     }
 
-    public function backup() : MediaInterface
+    public function store(UploadedFile $file, MimeType $mimeType)
+    {
+        $FileName       = $file->hashName();
+        $fileClientName = $file->getClientOriginalName();
+        $fileMimeType   = $file->getMimeType();
+
+        $mimeType = $mimeType->firstOrCreate(['value' => $fileMimeType]);
+
+        $this->mediaModel->create([
+            'client_name'  => $fileClientName,
+            'file_name'    => $FileName,
+            'mime_type_id' => $mimeType->id,
+        ]);
+
+        return $this;
+    }
+
+    public function backup()
     {
         return $this;
     }
 
-    public function optimize() : MediaInterface
+    public function optimize()
     {
         return $this;
     }
 
-    public function resize($width, $heigth = null) : MediaInterface
+    public function resize($width, $heigth = null)
     {
         return $this;
     }
 
-    public function toCollection($name) : MediaInterface
+    public function toCollection($name)
     {
         return $this;
     }
