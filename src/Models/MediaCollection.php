a5d34714d501076be145c46d0760ffc222d173fa
Eleandro Duzentos <eleandro@inbox.ru>
2018-12-24 22:43:30 +0100

    Added model relation

--- src/Models/MediaCollection.php
+++ src/Models/MediaCollection.php
@@ -2,9 +2,15 @@
 
 namespace e200\Mediavel\Models;
 
+use e200\Mediavel\Models\Media;
 use Illuminate\Database\Eloquent\Model;
 
 class MediaCollection extends Model
 {
     protected $fillable = ['name'];
+
+    public function medias()
+    {
+        return $this->hasMany(Media::class);
+    }
 }
