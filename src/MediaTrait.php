4eca834024559fbb4b7094871055cd0d0137b856
Eleandro <eleandro@inbox.ru>
2019-02-06 09:17:02 +0100

    Added mimeTypeFactory Added store implementation

--- src/MediaTrait.php
+++ src/MediaTrait.php
@@ -2,42 +2,50 @@
 
 namespace e200\Mediavel;
 
+use Laravel\Lumen\Http\Request;
 use Illuminate\Http\UploadedFile;
 use e200\Mediavel\Contracts\StorageInterface;
 use e200\Mediavel\Contracts\Factories\MediaFactoryInterface;
+use e200\Mediavel\Contracts\Factories\MimeTypeFactoryInterface;
 
 trait MediaTrait
 {
-    protected $media;
     protected $mediaFactory;
+    protected $mimeTypeFactory;
     protected $storage;
 
     public function __construct(
         MediaFactoryInterface $mediaFactory,
+        MimeTypeFactoryInterface $mimeTypeFactory,
         StorageInterface $storage
     ) {
         $this->mediaFactory = $mediaFactory;
+        $this->mimeTypeFactory = $mimeTypeFactory;
         $this->storage = $storage;
     }
 
     public function store(UploadedFile $uploadedFile, $diskName = null)
     {
-        $filePath = $this->storage->store($uploadedFile, $diskName);
+        $storedFilePath = $this->storage->store($uploadedFile, $diskName);
 
-        $media = $this->mediaFactory->makeFrom($uploadedFile, $filePath);
+        $fileName = $uploadedFile->hashName();
+        $fileClientName = $uploadedFile->getClientOriginalName();
+        $fileMimeType = $uploadedFile->getMimeType();
+
+        $mimeType = $this->mimeTypeFactory->make($fileMimeType);
+
+        $this->client_name = $fileClientName;
+        $this->file_path = $storedFilePath;
+        $this->mime_type_id = $mimeType->id;
+
+        $this->save();
 
         return $this;
     }
 
     public function preserveOriginal()
     {
-        $media = $this->media;
-
-        if (is_null($media)) {
-            // throwException
-        }
-
-        $media->getDirPath();
+        $this->getDirPath();
 
         return $this;
     }
