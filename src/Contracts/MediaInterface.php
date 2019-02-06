a871f5e72956ddc259cbf56ffc2aa3c150d8ac1f
Eleandro <eleandro@inbox.ru>
2019-02-06 09:18:41 +0100

    Removed contract methods

--- src/Contracts/MediaInterface.php
+++ src/Contracts/MediaInterface.php
@@ -6,13 +6,5 @@ use Illuminate\Http\UploadedFile;
 
 interface MediaInterface
 {
-    public function store(UploadedFile $file, $diskName = null);
-
-    public function preserveOriginal();
-
-    public function optimize();
-
-    public function resize($width, $heigth = null);
-
-    public function toCollection($name);
+    //
 }
