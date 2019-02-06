bf6557ae5c955156022939ce0120bf59e2c4814a
Eleandro <eleandro@inbox.ru>
2019-02-06 09:19:07 +0100

    Changed controller method from uplaod to add

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'e200\Mediavel\Http\Controllers'], function () {
-    Route::post('/mediavel/image', 'ImageController@resolve');
-    Route::post('/mediavel/file', 'FileController@resolve');
+    Route::post('/mediavel/image', 'ImageController@add');
+    Route::post('/mediavel/file', 'FileController@add');
 });
