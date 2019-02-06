9412b773f34cce050798a09d37534ea0f4da0216
Eleandro <eleandro@inbox.ru>
2019-02-06 09:23:11 +0100

    Extended base Controller class

--- src/Http/Controllers/FileController.php
+++ src/Http/Controllers/FileController.php
@@ -4,7 +4,7 @@ namespace e200\Mediavel\Http\Controllers;
 
 use Illuminate\Http\Request;
 use e200\Mediavel\FileHandler;
-use Illuminate\Routing\Controller;
+use App\Http\Controllers\Controller;
 
 class FileController extends Controller
 {
