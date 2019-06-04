c74c6d4e9d500d409e01820b3e9639b16b1624b7
Eleandro <eleandro@inbox.ru>
2019-06-04 15:55:50 +0100

    Added DiskStorage

--- src/Contracts/StorageInterface.php
+++ src/Contracts/StorageInterface.php
@@ -2,9 +2,10 @@
 
 namespace e200\Mediavel\Contracts;
 
+use e200\Mediavel\Models\Media;
 use Illuminate\Http\UploadedFile;
 
 interface StorageInterface
 {
-    public function store(UploadedFile $uploadedFil, $diskName);
+    public function store(UploadedFile $uploadedFile, $diskName) : Media;
 }
---
+++ src/DiskStorage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace e200\Mediavel;
+
+use e200\Mediavel\Models\Media;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use e200\Mediavel\PathGeneratorInterface;
+use e200\Mediavel\Contracts\StorageInterface;
+
+class DiskStorage implements StorageInterface
+{
+    public function store(UploadedFile $uploadedFile, $disk = null) : Media
+    {
+        if (is_null($disk)) {
+            $disk = config('mediavel.disks.default');
+        }
+
+        $fileName     = $uploadedFile->getClientOriginalName();
+        $filePath     = $uploadedFile->getPath();
+        $fileExt      = $uploadedFile->getClientOriginalExtension();
+        $fileMimeType = $uploadedFile->getMimeType();
+
+        $randomFileName = $this->generateFilename($fileExt);
+
+        $storagePath = $this->getStoragePath();
+
+        $uploadFilePath = $storagePath . DIRECTORY_SEPARATOR . $randomFileName;
+
+        Storage::disk($disk)->put($uploadFilePath, File::get($uploadedFile));
+
+        $userId = null;
+        $user = Auth::guard('api')->user();
+
+        if ($user) {
+            $userId = $user->id;
+        }
+
+        $media = Media::create([
+            'name'      => $fileName,
+            'path'      => $uploadFilePath,
+            'mime_type' => $fileMimeType,
+            'user_id'   => $userId
+        ]);
+
+        return $media;
+    }
+
+    public function getStoragePath()
+    {
+        $folderName = config('mediavel.folder_name');
+
+        $pathGeneratorClass = config('mediavel.path_generator');
+
+        $storagePath = app($pathGeneratorClass)->generate($folderName);
+
+        return $storagePath;
+    }
+
+    public function generateFilename($ext)
+    {
+        return uniqid(). '.' . $ext;
+    }
+}
