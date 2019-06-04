76926589c1d7620d85677781f871d06578d4616a
Eleandro <eleandro@inbox.ru>
2019-06-04 15:59:56 +0100

    Added MediaValidator

---
+++ src/Contracts/MediaValidatorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace e200\Mediavel\Contracts;
+
+use Illuminate\Http\Request;
+
+interface MediaValidatorInterface
+{
+    //
+}
---
+++ src/MediaValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace e200\Mediavel;
+
+use Illuminate\Http\Request;
+use e200\Mediavel\Contracts\MediaValidatorInterface;
+
+class MediaValidator implements MediaValidatorInterface
+{
+    public function validate(Request $request, $field)
+    {
+        $acceptedFileFormats = [
+            'jpeg', 'jpg', 'png',
+            'svg',  'ico', 'bmp',
+            'gif',  'pdf', 'doc',
+            'docx', 'xml'
+        ];
+
+        $acceptedFileFormatsString = implode(',', $acceptedFileFormats);
+
+        $validationRules = "bail|required|mimes:$acceptedFileFormatsString|max:5012";
+
+        $request->validate([
+            $field => $validationRules,
+        ]);
+    }
+}
